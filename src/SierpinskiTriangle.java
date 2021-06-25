c7b33c50dfa80177414664c189f7e7a7b5456ddf
Joey Smeets <joeysmeetsss@gmail.com>
2021-06-25 15:43:38 +0200

    Fully functional triangle. Added more comments.

--- src/SierpinskiTriangle.java
+++ src/SierpinskiTriangle.java
@@ -7,28 +7,36 @@ import java.awt.Polygon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+/**
+ * Lab 11: recursive triangle
+ * @author joeysmeets, anniengyn
+ * @version 25.06.2021
+ */
+
 public class SierpinskiTriangle extends JPanel {
 
 	// resolution of computer screen
-	int displayHeight = 2880;
-	int displayWidth = 1800;
+	private int displayHeight = 2880;
+	private int displayWidth = 1800;
 
 	// Color Array for go over when filling the triangles.
-		private static Color[] colours = { 
-				new Color(0xfff100), // yellow 
-				new Color(0xff8C00), // orange
-				new Color(0xe81123), // red
-				new Color(0xec008c), // pink
-				new Color(0x68217a), // purple
-				new Color(0x00188f), // navy
-				new Color(0x00bcf2), // blue
-				new Color(0x00b294), // turquoise
-				new Color(0x029658), // green
-				new Color(0xbad80a)};// chartreuse};
+	private static Color[] colours = { 
+			new Color(0xfff100), // yellow 
+			new Color(0xff8C00), // orange
+			new Color(0xe81123), // red
+			new Color(0xec008c), // pink
+			new Color(0x68217a), // purple
+			new Color(0x00188f), // navy
+			new Color(0x00bcf2), // blue
+			new Color(0x00b294), // turquoise
+			new Color(0x029658), // green
+			new Color(0xbad80a)};// chartreuse
 
+	// maximum amount of depth for the recursion
 	private static int maxDepth = 9;
 
-	SierpinskiTriangle(Dimension d) {
+	// constructor
+	public SierpinskiTriangle(Dimension d) {
 		setSize(d);
 	}
 
@@ -44,15 +52,19 @@ public class SierpinskiTriangle extends JPanel {
 		frame.setVisible(true);
 	}
 
+	// main method
 	public static void main(String[] args) {
 		SierpinskiTriangle t = new SierpinskiTriangle();
-
 	}
 
 	/**
 	 * Draws a simple triangle 
 	 * //Sources: https://stackoverflow.com/questions/25835468/draw-a-triangle-from-scratch-given-three-points
 	 *            https://www.delftstack.com/de/howto/java/java-draw-triangle/
+	 * @param a
+	 * @param b
+	 * @param c
+	 * @param triangle
 	 */
 
 	public void drawTriangle(Point a, Point b, Point c, Graphics triangle) {
@@ -62,7 +74,11 @@ public class SierpinskiTriangle extends JPanel {
 		triangle.drawLine(c.x, c.y, b.x, b.y); // bottom line
 
 	}
-
+	
+	/**
+	 * Paint method for on the frame
+	 * @param triangle
+	 */
 	public void paint(Graphics triangle) {
 
 		// draws triangle on maximum display screen
@@ -80,14 +96,17 @@ public class SierpinskiTriangle extends JPanel {
 
 		drawTriangle(c, a, b, triangle);
 		drawRecursively(triangle, maxDepth, a, b, c);
-
 	}
 	
-	/*
+	/**
 	 * Draws recursive triangles by using midpoint calculation 
 	 * Sets / fills colour from given colour array
+	 * @param triangle
+	 * @param maxDepth
+	 * @param a
+	 * @param b
+	 * @param c
 	 */
-
 	public void drawRecursively(Graphics triangle, int maxDepth, Point a, Point b, Point c) {
 
 		Polygon p = new Polygon();
@@ -98,6 +117,7 @@ public class SierpinskiTriangle extends JPanel {
 		Point ab = midpoint(a, b);
 		Point bc = midpoint(b, c);
 		Point ca = midpoint(c, a);
+		
 		triangle.setColor(colours[maxDepth]);
 		triangle.fillPolygon(p);
 
@@ -108,12 +128,13 @@ public class SierpinskiTriangle extends JPanel {
 		}
 	}
 	
-	/*
+	/**
 	 * Calculates midpoints of Point a and Point b
+	 * @param a
+	 * @param b
+	 * @return midpoint of the points
 	 */
-
 	public Point midpoint(Point a, Point b) {
 		return new Point((a.x + b.x) / 2, (a.y + b.y) / 2);
 	}
-
 }
